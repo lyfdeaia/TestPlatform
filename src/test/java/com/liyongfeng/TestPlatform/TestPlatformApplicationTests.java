274b8101030ea79ea708594f4a4f07b3f3c57838
heqiyong <heqiyong@medlinker.com>
2020-04-17 15:28:26 +0800

    first commit

---
+++ src/test/java/com/liyongfeng/TestPlatform/TestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyongfeng.TestPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

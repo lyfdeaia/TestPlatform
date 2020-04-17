b23506742b6a77a279a67d45c109d9b27c51f630
heqiyong <heqiyong@medlinker.com>
2020-04-17 16:29:36 +0800

    添加日志模块

--- src/main/java/com/liyongfeng/TestPlatform/TestPlatformApplication.java
+++ src/main/java/com/liyongfeng/TestPlatform/TestPlatformApplication.java
@@ -1,13 +1,21 @@
 package com.liyongfeng.TestPlatform;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class TestPlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestPlatformApplication.class, args);
+//		SpringApplication.run(TestPlatformApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(TestPlatformApplication.class, args);
+		int length = context.getBeanDefinitionNames().length;
+		log.info("Spring boot启动初始化了{}个Bean", length);
+		log.error("Spring boot启动初始化了{}个Bean", length);
+
 	}
 
 }
